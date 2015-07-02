eba04388a4a4b8817283d4656c75d3a4a7d27b02
labby <rp@cms-lab.com>
2015-07-02 09:49:57 +0200

    modify info.php

--- info.php
+++ info.php
@@ -19,9 +19,12 @@ $module_function	= 'page';
 $module_version		= '1.1.10';
 $module_platform	= '2.0.0';
 $module_author		= 'Frank Heyne, Dietrich Roland Pehlke (last)';
-$module_license		= 'GNU General Public License';
-$module_requirements	= 'Works best under the Lepton CMS.';
+$module_license		= '<a href="http://www.gnu.org/licenses/gpl.htm">GNU General Public License</a>';
 $module_guid			= '3FFE634D-D6BD-4C42-B449-D7A1DE3BA74A';
 $module_description 	= 'This module allows you to create customised online forms.';
-$module_home			= 'http://wbdemo.heysoft.de/pages/en/mpform.php';
+$module_home			= '<a href="http://lepton-cms.com">LEPAdoR</a>';
+
+/**
+ * Documentation:  http://wbdemo.heysoft.de/pages/en/mpform.php
+ */
 ?>
