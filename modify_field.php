c89273cab9cd79386affd1e607844400f9c5cc4b
labby <rp@cms-lab.com>
2017-08-21 15:34:05 +0200

    add some classes

--- modify_field.php
+++ modify_field.php
@@ -98,7 +98,7 @@ switch ($type) {
 	case 'heading':
 		$fieldtypeoption = "<tr>\n"
 		."<td valign='top'>". $TEXT['TEMPLATE'] ."</td>\n"
-		."<td><textarea name='template'  cols='50' rows='5' style='width: 98%; height: 200px;'>". $form['extra'] ."</textarea></td>\n"
+		."<td><textarea class='mp_options' name='template'  cols='50' rows='5' style='width: 98%; height: 200px;'>". $form['extra'] ."</textarea></td>\n"
 		."</tr>\n";
 		$form['required'] = 3;
 		break;
@@ -108,11 +108,11 @@ switch ($type) {
 	case 'email_subj':
 		$fieldtypeoption = "<tr>\n"
 		."<td>". $TEXT['LENGTH'] .":</td>\n"
-		."<td><input type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
+		."<td><input class='mp_options' type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
 		."</tr>\n"
 		."<tr>\n"
 		."<td>". $MOD_MPFORM['backend']['TXT_DEFAULT'] .":</td>\n"
-		."<td><input type='text' name='value' value='". $form['value'] ."' style='width: 98%;' /></td>\n"
+		."<td><input class='mp_options' type='text' name='value' value='". $form['value'] ."' style='width: 98%;' /></td>\n"
 		."</tr>\n";
 		break;
 	case 'textarea':
@@ -129,48 +129,48 @@ switch ($type) {
 		}
 		$fieldtypeoption = "<tr>\n"
 		."<td>". $TEXT['WIDTH'] .":</td>\n"
-		."<td><input type='text' name='width' value='$cols' style='width: 98%;' maxlength='3' /></td>\n"
+		."<td><input class='mp_options' type='text' name='width' value='$cols' style='width: 98%;' maxlength='3' /></td>\n"
 		."</tr>\n"
 		."<tr>\n"
 		."<td>". $MOD_MPFORM['backend']['ROWS'] .":</td>\n"
-		."<td><input type='text' name='rows' value='$rows' style='width: 98%;' maxlength='3' /></td>\n"
+		."<td><input class='mp_options' type='text' name='rows' value='$rows' style='width: 98%;' maxlength='3' /></td>\n"
 		."</tr>\n"
 		."<tr>\n"
 		."<td>". $MOD_MPFORM['backend']['TXT_DEFAULT'] .":</td>\n"
-		."<td><textarea name='value' cols='50' rows='5' style='width: 98%; height: 100px;'>". $form['value'] ."</textarea></td>\n"
+		."<td><textarea class='mp_options' name='value' cols='50' rows='5' style='width: 98%; height: 100px;'>". $form['value'] ."</textarea></td>\n"
 		."</tr>\n";
 		break;
 	case 'html':
 		$fieldtypeoption = "<tr>\n"
 		."<td>". $MOD_MPFORM['backend']['HTML'] .":</td>\n"
-		."<td><textarea name='value' cols='80' rows='8' style='width: 98%; height: 200px;'>". $form['value'] ."</textarea></td>\n"
+		."<td><textarea class='mp_options' name='value' cols='80' rows='8' style='width: 98%; height: 200px;'>". $form['value'] ."</textarea></td>\n"
 		."</tr>\n";
 		$form['required'] = 3;
 		break;
 	case 'date':
 		$fieldtypeoption = "<tr>\n"
 		."<td>". $TEXT['LENGTH'] .":</td>\n"
-		."<td><input type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
+		."<td><input class='mp_options' type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
 		."</tr>\n"
 		."<tr>\n"
 		."<td>". $MOD_MPFORM['backend']['TXT_DEFAULT'] .":</td>\n"
-		."<td><input type='text' name='value' value='". $form['value'] ."' style='width: 98%;' /></td>\n"
+		."<td><input class='mp_options' type='text' name='value' value='". $form['value'] ."' style='width: 98%;' /></td>\n"
 		."</tr>\n";
 		break;
 	case 'email':
 		$fieldtypeoption = "<tr>\n"
 		."<td>". $TEXT['LENGTH'] .":</td>\n"
-		."<td><input type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
+		."<td><input class='mp_options' type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
 		."</tr>\n"
 		."<tr>\n"
 		."<td>". $MOD_MPFORM['backend']['TXT_DEFAULT'] .":</td>\n"
-		."<td><input type='text' name='value' value='". $form['value'] ."' style='width: 98%;' /></td>\n"
+		."<td><input class='mp_options' type='text' name='value' value='". $form['value'] ."' style='width: 98%;' /></td>\n"
 		."</tr>\n";
 		break;
 	case 'filename':
 		$fieldtypeoption = "<tr>\n"
 		."<td>". $TEXT['LENGTH'] .":</td>\n"
-		."<td><input type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
+		."<td><input class='mp_options' type='text' name='length' value='". $form['extra'] ."' style='width: 98%;' maxlength='3' /></td>\n"
 		."</tr>\n";
 		break;
 	case 'select':
@@ -197,8 +197,8 @@ switch ($type) {
 			($type == 'radio') ? $isdef = "isdefault" : $isdef = "isdefault$option_count";
 			$fieldtypeoption .= '<table cellpadding="3" cellspacing="0" width="100%" border="0">'
 				."<tr>\n<td width='70'>". $TEXT['OPTION'].' '.$option_count .":</td>\n"
-				."<td><input type='text' name='value$option_count' value='$ovalue' style='width: 250px;' /> "
-				."<input type='$kind' name='$isdef' value='$option_count' $cv /></td>\n"
+				."<td><input class='mp_options' type='text' name='value$option_count' value='$ovalue' style='width: 250px;' /> "
+				."<input class='mp_options' type='$kind' name='$isdef' value='$option_count' $cv /></td>\n"
 				."</tr></table>\n";
 		}
 		for($i = 0; $i < 2; $i++) {
@@ -206,11 +206,11 @@ switch ($type) {
 			($type == 'radio') ? $isdef = "isdefault" : $isdef = "isdefault$option_count";
 			$fieldtypeoption .= '<table cellpadding="3" cellspacing="0" width="100%" border="0">'
 				."<tr>\n<td width='70'>". $TEXT['OPTION'].' '.$option_count .":</td>\n"
-				."<td><input type='text' name='value$option_count' value='' style='width: 250px;' /> "
-				."<input type='$kind' name='$isdef' value='$option_count' /></td>\n"
+				."<td><input class='mp_options' type='text' name='value$option_count' value='' style='width: 250px;' /> "
+				."<input class='mp_options' type='$kind' name='$isdef' value='$option_count' /></td>\n"
 				."</tr></table>\n";
 			}
-		$fieldtypeoption .= "<input type='hidden' name='list_count' value='$option_count' /></td>\n</tr>\n";
+		$fieldtypeoption .= "<input class='mp_options' type='hidden' name='list_count' value='$option_count' /></td>\n</tr>\n";
 		break;
 	case 'fieldsetstart':
 	case 'fieldsetend':
@@ -223,15 +223,15 @@ switch ($type) {
 if($type == 'select') {
 	$form['extra'] = explode(',',$form['extra']);
 	$fieldtypeoption .= "<tr>\n<td>". $TEXT['SIZE'] .":</td>\n";
-	$fieldtypeoption .= '<td><input type="text" name="size" value="'. trim($form['extra'][0]) .'" style="width: 98%;" maxlength="3" /></td>';
+	$fieldtypeoption .= '<td><input class="mp_options" type="text" name="size" value="'. trim($form['extra'][0]) .'" style="width: 98%;" maxlength="3" /></td>';
 	$fieldtypeoption .= "\n</tr>\n<tr>\n";
 	$fieldtypeoption .= "<td>". $TEXT['ALLOW_MULTIPLE_SELECTIONS'] .":</td>\n";
-	$fieldtypeoption .= '<td><input type="radio" name="multiselect" id="multiselect_true" value="multiple"';
+	$fieldtypeoption .= '<td><input class="mp_options" type="radio" name="multiselect" id="multiselect_true" value="multiple"';
 	if($form['extra'][1] == 'multiple') $fieldtypeoption .= ' checked="checked"';
 	$fieldtypeoption .= " />";
 	$fieldtypeoption .= '<a href="#" onclick="javascript: document.getElementById(\'multiselect_true\').checked = true;">';
 	$fieldtypeoption .= $TEXT['YES'] ."</a>	&nbsp; ";
-	$fieldtypeoption .= '<input type="radio" name="multiselect" id="multiselect_false" value=""';
+	$fieldtypeoption .= '<input class="mp_options" type="radio" name="multiselect" id="multiselect_false" value=""';
 	if($form['extra'][1] == '') $fieldtypeoption .= ' checked="checked"';
 	$fieldtypeoption .= " />";
 	$fieldtypeoption .= '<a href="#" onclick="javascript: document.getElementById(\'multiselect_false\').checked = true;">';
@@ -239,27 +239,27 @@ if($type == 'select') {
 }
 if($type == 'checkbox' OR $type == 'radio') { 
 	$fieldtypeoption .= "<tr>\n<td valign='top'>". $TEXT['SEPERATOR'] .":</td>\n";
-	$fieldtypeoption .= '<td><input type="text" name="seperator" value="'. $form['extra'] .'" style="width: 98%;" />'."</td>\n</tr>\n";
+	$fieldtypeoption .= '<td><input class="mp_options" type="text" name="seperator" value="'. $form['extra'] .'" style="width: 98%;" />'."</td>\n</tr>\n";
 }
 if($type != 'heading' AND $type != 'fieldset_start' AND $type != 'fieldset_end' AND $type != 'none' AND $type != 'html') { 
 	$fieldtypeoption .= "<tr>\n<td>". $MOD_MPFORM['backend']['entry'] .":</td>\n";
-	$fieldtypeoption .= '<td><input type="radio" name="required" id="required_true" value="1"';
+	$fieldtypeoption .= '<td><input class="mp_options" type="radio" name="required" id="required_true" value="1"';
 	if($form['required'] == 1 OR $type == 'email_recip') $fieldtypeoption .= ' checked="checked"';
 	$fieldtypeoption .= " />";
 	$fieldtypeoption .= '<a href="#" onclick="javascript: document.getElementById(\'required_true\').checked = true;">';
 	$fieldtypeoption .= $MOD_MPFORM['backend']['compulsory_entry'] ."</a>	&nbsp; ";
-	$fieldtypeoption .= '<input type="radio" name="required" id="required_false" value="0"';
+	$fieldtypeoption .= '<input class="mp_options" type="radio" name="required" id="required_false" value="0"';
 	if($form['required'] == 0 AND $type != 'email_recip') $fieldtypeoption .= ' checked="checked"';
 	$fieldtypeoption .= " />";
 	$fieldtypeoption .= '<a href="#" onclick="javascript: document.getElementById(\'required_false\').checked = true;">';
 	$fieldtypeoption .= $MOD_MPFORM['backend']['optional_entry'] ."</a>	&nbsp; ";
-	$fieldtypeoption .= '<input type="radio" name="required" id="required_ro" value="2"';
+	$fieldtypeoption .= '<input class="mp_options" type="radio" name="required" id="required_ro" value="2"';
 	if($form['required'] == 2 AND $type != 'email_recip') $fieldtypeoption .= ' checked="checked"';
 	$fieldtypeoption .= " />";
 	$fieldtypeoption .= '<a href="#" onclick="javascript: document.getElementById(\'required_ro\').checked = true;">';
 	$fieldtypeoption .= $MOD_MPFORM['backend']['ro_entry'] ."</a></td>\n</tr>\n";
-	$fieldtypeoption .= "<tr>\n<td valign='top'>". $MENU['HELP'] .":</td>\n";
-	$fieldtypeoption .= '<td><textarea name="help"  cols="50" rows="5" style="width: 98%; height: 100px;">'. $form['help'] ."</textarea></td>\n</tr>\n";
+	$fieldtypeoption .= "<tr><td>&nbsp;</td></tr><tr class='helper'>\n<td valign='top'>". $MENU['HELP'] .":</td>\n";
+	$fieldtypeoption .= '<td><textarea class="mp_options" name="help"  cols="50" rows="5" style="width: 98%; height: 100px;">'. $form['help'] ."</textarea></td>\n</tr>\n";
 }
 
 
@@ -269,7 +269,7 @@ $form_values = array(
 	'PAGE_ID'		=> (int) $page_id,
 	'SECTION_ID'	=> (int) $section_id,
 	'FIELD_ID'		=> (int) $field_id,
-	'LEPTON_URL'		=> LEPTON_URL,
+	'LEPTON_URL'	=> LEPTON_URL,
 	'ADMIN_URL'		=> ADMIN_URL,
 	'TXT_SAVE'		=> $TEXT['SAVE'],
 	'TXT_CANCEL'	=> $TEXT['CANCEL'],
