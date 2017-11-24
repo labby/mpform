5a214435482c491a221624606a21e1fe760982cd
labby <rp@cms-lab.com>
2017-11-24 15:17:13 +0100

    code cleaning

--- view.php
+++ view.php
@@ -12,21 +12,30 @@
  *  
  *
  */
-
-// Must include code to stop this file being access directly
-if(defined('LEPTON_PATH') == false) { exit("Cannot access this file directly"); }
-
-// obtain module directory
-$mod_dir = basename(dirname(__FILE__));
+ 
+ // include class.secure.php to protect this file and the whole CMS!
+if (defined('LEPTON_PATH')) {	
+	include(LEPTON_PATH.'/framework/class.secure.php'); 
+} else {
+	$oneback = "../";
+	$root = $oneback;
+	$level = 1;
+	while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+		$root .= $oneback;
+		$level += 1;
+	}
+	if (file_exists($root.'/framework/class.secure.php')) { 
+		include($root.'/framework/class.secure.php'); 
+	} else {
+		trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+	}
+}
+// end include class.secure.php
 
 $MOD_MPFORM = (dirname(__FILE__))."/languages/". LANGUAGE .".php";
 require_once ( !file_exists($MOD_MPFORM) ? (dirname(__FILE__))."/languages/EN.php" : $MOD_MPFORM );
 
-$captcha_filename_lepton_1 = LEPTON_PATH."/include/captcha/captcha.php";
-require_once((file_exists($captcha_filename_lepton_1))
-	? $captcha_filename_lepton_1
-	: LEPTON_PATH.'/modules/captcha_control/captcha/captcha.php'
-);
+require_once LEPTON_PATH.'/modules/captcha_control/captcha/captcha.php';
 
 // define some vars required later in private.php
 global $success_email_text, $success_email_to, $felder;
@@ -39,11 +48,11 @@ if (file_exists(LEPTON_PATH .'/modules/mpform/private.php')) {
 // Work-out if the form has been submitted or not
 if ($_POST != array()) {    
 	// some form has been submitted:
-	include_once(LEPTON_PATH .'/modules/mpform/evalform.php');
+	require_once(LEPTON_PATH .'/modules/mpform/evalform.php');
 	eval_form($section_id);
 } else {
 	// the form has not been submitted:
-	include_once(LEPTON_PATH .'/modules/mpform/paintform.php');
+	require_once(LEPTON_PATH .'/modules/mpform/paintform.php');
 	paint_form($section_id);
 }
 ?>
