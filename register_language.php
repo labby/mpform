405daa93dcc8b9327d4612be74a03987a78bba34
labby <rp@cms-lab.com>
2018-02-21 13:20:48 +0100

    add register language

---
+++ register_language.php
@@ -0,0 +1,58 @@
+<?php
+
+/**
+ * @module          your module
+ * @author          LEPTON project
+ * @copyright       2015-2016 LEPTON project
+ * @link            http://www.LEPTON-cms.org
+ * @license         http://www.gnu.org/licenses/gpl.html
+ * @license_terms   please see LICENSE and COPYING files in your package
+ *
+ */
+ 
+ // include class.secure.php to protect this file and the whole CMS!
+if (defined('LEPTON_PATH')) {   
+   include(LEPTON_PATH.'/framework/class.secure.php');
+} else {
+   $oneback = "../";
+   $root = $oneback;
+   $level = 1;
+   while (($level < 10) && (!file_exists($root.'/framework/class.secure.php'))) {
+      $root .= $oneback;
+      $level += 1;
+   }
+   if (file_exists($root.'/framework/class.secure.php')) {
+      include($root.'/framework/class.secure.php');
+   } else {
+      trigger_error(sprintf("[ <b>%s</b> ] Can't include class.secure.php!", $_SERVER['SCRIPT_NAME']), E_USER_ERROR);
+   }
+}
+// end include class.secure.php
+ 
+ 
+require_once('info.php'); 
+if (isset ($wb)) {
+	/**
+	 *	load the correct language-file for frontend
+	 */
+	if (file_exists (LEPTON_PATH.'/templates/'.DEFAULT_TEMPLATE.'/frontend/'.$module_directory.'/'.LANGUAGE.'.php')) {
+			require_once (LEPTON_PATH.'/templates/'.DEFAULT_TEMPLATE.'/frontend/'.$module_directory.'/'.LANGUAGE.'.php');
+	}
+	else {
+		$lang = (dirname(__FILE__))."/languages/". LANGUAGE .".php";
+		require_once ( !file_exists($lang) ? (dirname(__FILE__))."/languages/EN.php" : $lang );
+	}
+}
+else {
+	/**
+	 *	load the correct language-file for backend
+	 */
+	if (file_exists (LEPTON_PATH.'/templates/'.DEFAULT_THEME.'/backend/'.$module_directory.'/'.LANGUAGE.'.php')) {
+			require_once (LEPTON_PATH.'/templates/'.DEFAULT_THEME.'/backend/'.$module_directory.'/'.LANGUAGE.'.php');
+	}
+	else {
+		$lang = (dirname(__FILE__))."/languages/". LANGUAGE .".php";
+		require_once ( !file_exists($lang) ? (dirname(__FILE__))."/languages/EN.php" : $lang );
+	}
+}
+?>
