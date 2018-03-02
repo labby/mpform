2cd60979d2cb52a9535b0edc96b037e65afea37a
aldus <drp@cms-lab.com>
2018-03-02 12:32:01 +0100

    Add some comments.

--- classes/mpform.php
+++ classes/mpform.php
@@ -15,13 +15,37 @@
 
 class mpform extends LEPTON_abstract
 {
+    /**
+     *  The instance of this class.
+     *  @var    instance
+     */
     public static $instance;
-    
+
+    /**
+     *  Initial call from the parent.
+     *  Startpoint of all we need to do inside this module-class.
+     */    
     public function initialize()
     {
     
     }
- 
+
+    /**
+     *  Generates a full URL to a page via a section_id.
+     *  The url includes also the #language param and the section anchor to the given section id.
+     *
+     *  @access static
+     *  @param  integer $iSection_id    A valid section id. 
+     *  @return string  The full absulut path to the page where the given section (-i) is used.
+     *
+     *  @code{.php}
+     *
+     *      mpform::build_action_url( 1234 );
+     *      // will return something like
+     *      // https:://www.all_the_best.tld/pages/examples/my-form.php?lang=RU#lep_1234
+     *
+     *  @endcode
+     */ 
 	public static function build_action_url ($iSection_id = -1) {
 		$database = LEPTON_database::getInstance();
 		$page_id = $database->get_one("SELECT page_id FROM ".TABLE_PREFIX."sections WHERE section_id = ".$iSection_id." ");
@@ -37,6 +61,13 @@ class mpform extends LEPTON_abstract
 		return LEPTON_URL.PAGES_DIRECTORY.$page_values['link'].PAGE_EXTENSION.'?lang='.LANGUAGE."#".SEC_ANCHOR."section_".$section_id;	
 	}
 	
+	/**
+	 *  Static function for internal use within TWIG to trransform given values for Twig.
+	 *
+	 *  @param  array $all_fields   Pass by reference!
+	 *  @return void    As the param-value is passed by reference.
+	 *
+	 */
 	static public function prepare_fields( &$all_fields )
 	{
 		foreach($all_fields as &$field)
